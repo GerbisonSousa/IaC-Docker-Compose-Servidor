4260ed787d94d63bf099c0d12f4a7c88fbe70b66
Gerbison Sousa <92000084+GerbisonSousa@users.noreply.github.com>
2022-12-03 09:26:44 -0300

    Add files via upload

---
+++ IaC-Docker.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+apt install update
+cd ..
+mkdir data
+cd /data
+mkdir apache
+cd /apache
+mv index.html
+
+cd ../..
+mv docker-compose.yml
+
+sudo apt install -y docker.io
+sudo apt-get install -y docker-compose
+
+echo "------------------------"
+
+docker pull ubuntu
+docker pull apache2
+docker run -dti --name iac-apache ubuntu
+echo "Criando a imagem iac-apache versão 1.0!"
+docker image build -t iac-apache:1.0 .
+echo "Chamada da imagem docker iac-apache para execução! "
+docker run -ti --name aplic-web iac-apache:1.0
+
+echo "Chamada do docker-compose.yml para execução! "
+
+docker-compose up
